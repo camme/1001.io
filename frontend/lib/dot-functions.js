73b127a2e060afdf0dba89db6f0fb0efc708c42a
camilo tapia <camilo.tapia@gmail.com>
2013-12-30 02:05:01 +0100

    analytics

--- frontend/lib/dot-functions.js
+++ frontend/lib/dot-functions.js
@@ -80,6 +80,18 @@ module.exports = {
 
     scriptsFolder: function() {
         return config.environment == "production" ? "/scripts-production" : "/scripts";
+    },
+
+    analytics: function() {
+        var script = "<script>";
+        script += "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){";
+        script += "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
+        script += "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)";
+        script += "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
+        script += "ga('create', '" + config.analytics + "', '1001.io');";
+        script += "ga('send', 'pageview');";
+        script += "</script>";
+        return config.environment == "production" ? script : "";
     }
 
 };
